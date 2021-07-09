9ba618e44d3d40f8d9281ee5bbe76a2bbe77ca9d
bootzmanuva <rugenr@gmail.com>
2021-07-09 15:31:27 -0700

    Initial commit

---
+++ Assets/Scripts/Attacker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attacker : MonoBehaviour
+{
+    float currentSpeed = 1f;
+    [SerializeField] int health = 100;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.left * currentSpeed * Time.deltaTime);
+    }
+
+    public void SetMovementSpeed(float speed)
+    {
+        currentSpeed = speed;
+    }
+}
